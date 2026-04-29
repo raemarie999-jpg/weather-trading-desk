1dba545c1f962ff85eff37ca9a7ef782d8fc3b7a
raemarie999-jpg <raemarie999@gmail.com>
2026-04-29 18:16:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ headers = {
     "X-API-Key": API_KEY
 }
 
-run_id = "019ddb23-e09f-722e-8094-0983a865ee6f"
+stations = ["KDFW", "KORD"]
 
-url = f"https://api.minutetemp.com/api/v1/forecast/runs/{run_id}"
+for station in stations:
+    url = f"https://api.minutetemp.com/api/v1/stations/{station}/forecast/runs"
+    r = requests.get(url, headers=headers)
 
+    print("======")
+    print(station)
+    print("STATUS:", r.status_code)
+    print(r.text[:1000])
 r = requests.get(url, headers=headers)
 
 print(r.status_code)
